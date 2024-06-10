6e74fad67c40ba77a565f5418dbd13e2a9cc79c7
Lucas Sekwati <luxturesekwati@gmail.com>
2024-06-10 22:37:59 +0200

    "Not found"

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Flask App instance
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """ Close storage """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Custom 404 error handler """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
