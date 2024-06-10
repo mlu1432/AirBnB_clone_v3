a2a754f649f348e74637b3ac70c9385477696aa0
Lucas Sekwati <luxturesekwati@gmail.com>
2024-06-10 21:39:54 +0200

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-""" Flask app for hbnb """
-from flask import Flask, make_response, jsonify
+"""
+Flask App instance
+"""
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
-""" Cors access to selected resources from a different origin."""
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Returns JSON error repsponse"""
-    return make_response(jsonify({"error": "Not found"}), 404)
-
 
 @app.teardown_appcontext
-def teardown(error):
-    """closes storage"""
+def teardown(exception):
+    """ Close storage """
     storage.close()
 
-
 if __name__ == "__main__":
-    """main function"""
-    host = getenv("HBNB_API_HOST", default="0.0.0.0")
-    port = getenv("HBNB_API_PORT", default=5000)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python3
-""" indexapi """
-from flask import Blueprint, jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+"""
+Index module
+"""
 from api.v1.views import app_views
+from flask import jsonify
 
-
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
-    """status of api"""
+    """ Returns the status of the API """
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objects = {}
-    for i in range(len(classes)):
-        num_objects[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objects)
